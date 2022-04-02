ad75143982810c1d15612cb622364b220ac6b349
loiclecolier <loic.lecolier@outlook.be>
2022-04-02 13:04:50 +0200

    Reportage Notélé Section

--- src/Containers/Notele/Notele.js
+++ src/Containers/Notele/Notele.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Notele.css'
+import Reportage from './reportage-notele.mp4'
+import PosterReportage from './PosterReportage.png'
 
 export default function Notele() {
   return (
     <div id="reportage-notele">
-
+      <video className="video-reportage-notele" controls preload="none" poster={PosterReportage}>
+        <source src={Reportage} type="video/mp4" />
+      </video>
     </div>
   )
 }
